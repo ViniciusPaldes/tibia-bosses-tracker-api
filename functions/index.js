33efdc93b40658b3682de08c8ac302ee1b21540f
Vinicius Paldês <viniciuspaldes@Viniciuss-MacBook-Pro.local>
2023-07-21 15:15:48 -0300

    Killed yesterday endpoint and function

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,24 @@ const cleanText = (text) => {
   return text.replace(/\n/g, '').replace(/days? ago/g, '').trim();
 };
 
+const getKilledYesterday = async () => {
+  try {
+    const url = 'https://www.tibia-statistic.com/bosshunter/details/venebra';
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const figcaptions = $('figcaption');
+    return figcaptions.map((index, element) => {
+      return { 'name': cleanText($(element).text()) };
+    }).get();
+  } catch (error) {
+    console.error('Error:', error.message);
+    return [];
+  }
+};
+
+    
 const getBossList = async () => {
   try {
     const url = 'https://www.tibia-statistic.com/bosshunter/details/venebra';
@@ -60,6 +78,18 @@ app.get('/tibia-statistics', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+app.get('/killed-yesterday', async (req, res) => {
+  try {
+    const killedYesterday = await getKilledYesterday();
+    res.json(killedYesterday);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
 
 
 
