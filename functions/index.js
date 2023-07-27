58fb95ae62840c8530bf5c272bb637f90a2daf71
Vinicius Paldês <viniciuspaldes@Viniciuss-MacBook-Pro.local>
2023-07-27 20:54:16 -0300

    Guild stats get boss list

--- functions/index.js
+++ functions/index.js
@@ -67,11 +67,53 @@ const getBossList = async () => {
   }
 };
 
+async function getGuildStatsBossList() {
+  try {
+    const url = 'https://guildstats.eu/bosses?world=Venebra&monsterName=&bossType=3&rook=0';
+
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const bossList = [];
+
+    $('#myTable tbody tr').each((index, element) => {
+      const name = $(element).find('td:nth-child(2)').text().trim();
+      const killedYesterday = parseInt($(element).find('td:nth-child(4)').text().trim());
+      const lastSeen = $(element).find('td:nth-child(8)').text().trim();
+      const chanceText = $(element).find('td:nth-child(11)').text().trim();
+      const expectedIn = $(element).find('td:nth-child(12)').text().trim();
+      let chance = chanceText;
+
+      // Handle 'No' and '1Low' cases
+      if (chanceText === '0No') {
+        chance = '0';
+      } else if (chanceText === '1Low') {
+        chance = '0.01';
+      } else {
+        if (chanceText !== '-1') {
+          chance = chanceText.replace('%', '0')
+          chance = chance / 100
+        }
+      }
+
+      // Exclude bosses with chance -1 from the list
+      if (chanceText !== '-1') {
+        bossList.push({ name, killedYesterday, lastSeen, chance, expectedIn: expectedIn || '0' });
+      }
+    });
+
+    return bossList;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+}
+
 
 app.get('/tibia-statistics', async (req, res) => {
   try {
     const bossList = await getBossList();
-    console.log("bossList", bossList)
     res.json(bossList);
   } catch (error) {
     console.error('Error:', error);
@@ -89,6 +131,15 @@ app.get('/killed-yesterday', async (req, res) => {
   }
 });
 
+app.get('/guild-stats', async (req, res) => {
+  try {
+    const bossList = await getGuildStatsBossList();
+    res.json(bossList);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 
 
