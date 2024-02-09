adf80a703c283b2ff261acb6b0a5164a66e35ccc
ViniciusPaldes <vinnydp@gmail.com>
2024-02-09 13:08:28 -0300

    Testing with more real human header

--- functions/tibia-wiki-creatures.js
+++ functions/tibia-wiki-creatures.js
@@ -24,9 +24,18 @@ function getDifficultyNumber(difficultyString) {
 async function fetchTibiaWikiCreaturesData(url) {
   try {
     // Fetch the HTML of the page
-    const { data } = await axios.get('https://www.tibiawiki.com.br');
-    // const $ = cheerio.load(data);
-    return [{name: 1, voice: 2}];
+    const { data } = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'DNT': '1', // Do Not Track Request Header
+        'Upgrade-Insecure-Requests': '1',
+      }
+    })
+    const $ = cheerio.load(data);
     const URL_PREFIX = "https://www.tibiawiki.com.br";
 
     // The array to hold our creatures data
