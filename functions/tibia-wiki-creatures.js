ce1a9470b7dbf4d6279a84073cb0713b0dd06b08
ViniciusPaldes <vinnydp@gmail.com>
2024-02-09 12:20:03 -0300

    Vercel integration to test api

--- functions/tibia-wiki-creatures.js
+++ functions/tibia-wiki-creatures.js
@@ -24,11 +24,7 @@ function getDifficultyNumber(difficultyString) {
 async function fetchTibiaWikiCreaturesData(url) {
   try {
     // Fetch the HTML of the page
-    const { data } = await axios.get(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-      },
-    });
+    const { data } = await axios.get(url);
     const $ = cheerio.load(data);
 
     const URL_PREFIX = "https://www.tibiawiki.com.br";
