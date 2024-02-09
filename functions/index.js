e8fc8bc890043e91d786b1eddde21988acb9c117
ViniciusPaldes <vinnydp@gmail.com>
2024-02-09 10:51:05 -0300

    Get all Tibia Creatures from Tibia Wiki creatures page

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,42 @@
 // Import the required modules
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const moment = require('moment');
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const moment = require("moment");
 
 // Create an instance of the Express application
 const app = express();
 const port = process.env.PORT || 3000; // Choose any port number you prefer
 
 const cleanText = (text) => {
-  return text.replace(/\n/g, '').replace(/days? ago/g, '').trim();
+  return text
+    .replace(/\n/g, "")
+    .replace(/days? ago/g, "")
+    .trim();
 };
 
 const getKilledYesterday = async () => {
   try {
-    const url = 'https://www.tibia-statistic.com/bosshunter/details/venebra';
+    const url = "https://www.tibia-statistic.com/bosshunter/details/venebra";
     const response = await axios.get(url);
     const html = response.data;
     const $ = cheerio.load(html);
 
-    const figcaptions = $('figcaption');
-    return figcaptions.map((index, element) => {
-      return { 'name': cleanText($(element).text()) };
-    }).get();
+    const figcaptions = $("figcaption");
+    return figcaptions
+      .map((index, element) => {
+        return { name: cleanText($(element).text()) };
+      })
+      .get();
   } catch (error) {
-    console.error('Error:', error.message);
+    console.error("Error:", error.message);
     return [];
   }
 };
 
-
 const getBossListKillStatistic = async () => {
   try {
-    const url = 'https://www.tibia-statistic.com/bosshunter/details/venebra';
+    const url = "https://www.tibia-statistic.com/bosshunter/details/venebra";
     const response = await axios.get(url);
     const html = response.data;
     const $ = cheerio.load(html);
@@ -41,8 +45,10 @@ const getBossListKillStatistic = async () => {
     let isSecondTable = false;
 
     // Replace 'table-hover' with the actual class name of the table
-    $('table.table-hover.table-dark.table-sm.table-bordered.table-striped.rounded tbody tr').each((index, element) => {
-      const tds = $(element).find('td');
+    $(
+      "table.table-hover.table-dark.table-sm.table-bordered.table-striped.rounded tbody tr"
+    ).each((index, element) => {
+      const tds = $(element).find("td");
       const name = cleanText($(tds[1]).text());
       const lastSeen = cleanText($(tds[2]).text());
 
@@ -53,7 +59,7 @@ const getBossListKillStatistic = async () => {
 
       // Process the data differently based on whether it's the first table or second table
       if (isSecondTable) {
-        bossList.push({ name, lastSeen, chance: 'No chance' });
+        bossList.push({ name, lastSeen, chance: "No chance" });
       } else {
         const chance = cleanText($(tds[3]).text());
         bossList.push({ name, chance, lastSeen });
@@ -63,14 +69,15 @@ const getBossListKillStatistic = async () => {
     console.log(bossList);
     return bossList;
   } catch (error) {
-    console.error('Error:', error.message);
+    console.error("Error:", error.message);
     return [];
   }
 };
 
 async function getGuildStatsBossList() {
   try {
-    const url = 'https://guildstats.eu/bosses?world=Venebra&monsterName=&bossType=3&rook=0';
+    const url =
+      "https://guildstats.eu/bosses?world=Venebra&monsterName=&bossType=3&rook=0";
 
     const response = await axios.get(url);
     const html = response.data;
@@ -78,50 +85,66 @@ async function getGuildStatsBossList() {
 
     const bossList = [];
 
-    $('#myTable tbody tr').each((index, element) => {
-      const name = $(element).find('td:nth-child(2)').text().trim();
-      const killedYesterday = parseInt($(element).find('td:nth-child(4)').text().trim());
-      const lastSeen = $(element).find('td:nth-child(8)').text().trim();
-      const chanceText = $(element).find('td:nth-child(11)').text().trim();
-      const expectedIn = $(element).find('td:nth-child(12)').text().trim();
+    $("#myTable tbody tr").each((index, element) => {
+      const name = $(element).find("td:nth-child(2)").text().trim();
+      const killedYesterday = parseInt(
+        $(element).find("td:nth-child(4)").text().trim()
+      );
+      const lastSeen = $(element).find("td:nth-child(8)").text().trim();
+      const chanceText = $(element).find("td:nth-child(11)").text().trim();
+      const expectedIn = $(element).find("td:nth-child(12)").text().trim();
 
       let chance = chanceText;
 
       // Handle 'No' and '1Low' cases
-      if (chanceText === '0No') {
-        chance = '0';
-      } else if (chanceText === '1Low') {
-        chance = '0.01';
+      if (chanceText === "0No") {
+        chance = "0";
+      } else if (chanceText === "1Low") {
+        chance = "0.01";
       } else {
-        if (chanceText !== '-1') {
-          chance = chanceText.replace('%', '0')
-          chance = chance / 100
+        if (chanceText !== "-1") {
+          chance = chanceText.replace("%", "0");
+          chance = chance / 100;
         }
       }
 
       // Exclude bosses with chance -1 from the list
-      if (chanceText !== '-1') {
-        bossList.push({ name, killedYesterday, lastSeen, chance, expectedIn: expectedIn || '0' });
+      if (chanceText !== "-1") {
+        bossList.push({
+          name,
+          killedYesterday,
+          lastSeen,
+          chance,
+          expectedIn: expectedIn || "0",
+        });
       }
     });
 
     return bossList;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
     return [];
   }
 }
 
 async function getDuplicatedBosses(url) {
   try {
-    const url = 'https://www.tibiabosses.com/stats/?world=Venebra&mode=3';
+    const url = "https://www.tibiabosses.com/stats/?world=Venebra&mode=3";
 
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
 
     const bosses = [
-      { name: 'White Pale', cities: ['Edron', 'Darashia', 'Liberty Bay'], count: 3 },
-      { name: 'Rotworm Queen', cities: ["Darashia", "Ab'dendriel", "Edron", "Liberty Bay"], count: 4 },
+      {
+        name: "White Pale",
+        cities: ["Edron", "Darashia", "Liberty Bay"],
+        count: 3,
+      },
+      {
+        name: "Rotworm Queen",
+        cities: ["Darashia", "Ab'dendriel", "Edron", "Liberty Bay"],
+        count: 4,
+      },
     ];
 
     const result = [];
@@ -135,76 +158,174 @@ async function getDuplicatedBosses(url) {
         const lastSeen = $(el).text().trim();
         const chanceDiv = $(el).next();
         // Get the chance value based on the color
-        let chanceText = chanceDiv.css('color');
+        let chanceText = chanceDiv.css("color");
         let chances = 0;
 
-        if (chanceText === 'green') {
+        if (chanceText === "green") {
           chances = 1;
-        } else if (chanceText === 'blue') {
+        } else if (chanceText === "blue") {
           chances = 0.01;
         } else {
           chances = 0;
         }
 
-        result.push({ name: boss.name, city: city, lastSeen: lastSeen, chance: chances });
-
+        result.push({
+          name: boss.name,
+          city: city,
+          lastSeen: lastSeen,
+          chance: chances,
+        });
       });
     });
 
     return result;
   } catch (error) {
-    console.error('Error fetching data:', error.message);
+    console.error("Error fetching data:", error.message);
     return [];
   }
 }
 
+function getDifficultyNumber(difficultyString) {
+  switch (difficultyString) {
+    case "Inofensiva":
+      return 0;
+    case "Trivial":
+      return 1;
+    case "Fácil":
+      return 2;
+    case "Mediana":
+      return 3;
+    case "Difícil":
+      return 4;
+    case "Desafiador":
+      return 5;
+    default:
+      return -1;
+  }
+}
 
+async function fetchTibiaWikiCreaturesData(url) {
+  try {
+    // Fetch the HTML of the page
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
 
+    const URL_PREFIX = "https://www.tibiawiki.com.br";
 
+    // The array to hold our creatures data
+    const creatures = [];
 
+    // Find each table by ID and loop through its rows
+    $('table#tabelaDPL').first().each((_, table) => {
+      // Loop through each row of the table
+      $("tr", table).each((_, tr) => {
+        const tds = $("td", tr).filter((index, td) => index < $("td", tr).length - 1);
+        if (tds.length > 0) {
+          // Ensure there are columns to process
+          const creature = {
+            name: $(tds[0]).text().trim(),
+            image: `${URL_PREFIX}${$(tds[1]).find("img").attr("src") || ""}`,
+            hp: parseInt($(tds[2]).text().trim(), 10),
+            xp: parseInt($(tds[3]).text().trim(), 10),
+            charms: parseInt($(tds[4]).text().trim(), 10),
+            difficultyString: $(tds[5]).find("img").attr("title"),
+            difficulty: getDifficultyNumber($(tds[5]).find("img").attr("title")),
+          };
+          creatures.push(creature);
+        }
+      });
+    });
 
-app.get('/tibia-statistics', async (req, res) => {
+    // Log or return the JSON data
+    // console.log(JSON.stringify(creatures, null, 2));
+    return creatures;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+async function getTopTrumps() {
+  const urls = [
+    { url: "https://www.tibiawiki.com.br/wiki/Anf%C3%ADbios" },
+    { url: "https://www.tibiawiki.com.br/wiki/Aqu%C3%A1ticos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Aves" },
+    { url: "https://www.tibiawiki.com.br/wiki/Constructos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Criaturas_M%C3%A1gicas" },
+    { url: "https://www.tibiawiki.com.br/wiki/Dem%C3%B4nios" },
+    { url: "https://www.tibiawiki.com.br/wiki/Drag%C3%B5es" },
+    { url: "https://www.tibiawiki.com.br/wiki/Elementais" },
+    { url: "https://www.tibiawiki.com.br/wiki/Extra_Dimensionais" },
+    { url: "https://www.tibiawiki.com.br/wiki/Fadas" },
+    { url: "https://www.tibiawiki.com.br/wiki/Gigantes" },
+    { url: "https://www.tibiawiki.com.br/wiki/Humanos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Human%C3%B3ides" },
+    { url: "https://www.tibiawiki.com.br/wiki/Licantropos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Mam%C3%ADferos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Mortos-Vivos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Plantas_(Criatura)" },
+    { url: "https://www.tibiawiki.com.br/wiki/R%C3%A9pteis" },
+    { url: "https://www.tibiawiki.com.br/wiki/Slimes" },
+    { url: "https://www.tibiawiki.com.br/wiki/Vermes" },
+  ]
+
+  try {
+    const promises = urls.map(item => fetchTibiaWikiCreaturesData(item.url));
+    const results = await Promise.all(promises);
+    return results.flat();
+  } catch (error) {
+    console.error("Error fetching creature data:", error);
+  }
+}
+
+app.get("/tibia-statistics", async (req, res) => {
   try {
     const bossList = await getBossListKillStatistic();
     res.json(bossList);
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.get('/killed-yesterday', async (req, res) => {
+app.get("/killed-yesterday", async (req, res) => {
   try {
     const killedYesterday = await getKilledYesterday();
     res.json(killedYesterday);
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.get('/guild-stats', async (req, res) => {
+app.get("/guild-stats", async (req, res) => {
   try {
     const bossList = await getGuildStatsBossList();
     res.json(bossList);
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.get('/duplicated-bosses', async (req, res) => {
+app.get("/top-trumps", async (req, res) => {
+  try {
+    const data = await getTopTrumps();
+    res.json(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/duplicated-bosses", async (req, res) => {
   try {
     const bossList = await getDuplicatedBosses();
     res.json(bossList);
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
-
-
-
 
 // Start the server
 app.listen(port, () => {
---
+++ functions/tibia-wiki-creatures.js
@@ -0,0 +1,120 @@
+// Import the required modules
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+function getDifficultyNumber(difficultyString) {
+  switch (difficultyString) {
+    case "Inofensiva":
+      return 0;
+    case "Trivial":
+      return 1;
+    case "Fácil":
+      return 2;
+    case "Mediana":
+      return 3;
+    case "Difícil":
+      return 4;
+    case "Desafiador":
+      return 5;
+    default:
+      return -1;
+  }
+}
+
+async function fetchTibiaWikiCreaturesData(url) {
+  try {
+    // Fetch the HTML of the page
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+
+    const URL_PREFIX = "https://www.tibiawiki.com.br";
+
+    // The array to hold our creatures data
+    const creatures = [];
+
+    // Find each table by ID and loop through its rows
+    $("table#tabelaDPL")
+      .first()
+      .each((_, table) => {
+        // Loop through each row of the table
+        $("tr", table).each((_, tr) => {
+          const tds = $("td", tr).filter(
+            (index, td) => index < $("td", tr).length - 1
+          );
+          if (tds.length > 0) {
+            // Ensure there are columns to process
+            const creature = {
+              name: $(tds[0]).text().trim(),
+              image: `${URL_PREFIX}${$(tds[1]).find("img").attr("src") || ""}`,
+              hp: parseInt($(tds[2]).text().trim(), 10),
+              xp: parseInt($(tds[3]).text().trim(), 10),
+              charms: parseInt($(tds[4]).text().trim(), 10),
+              difficultyString: $(tds[5]).find("img").attr("title"),
+              difficulty: getDifficultyNumber(
+                $(tds[5]).find("img").attr("title")
+              ),
+            };
+            creatures.push(creature);
+          }
+        });
+      });
+
+    // Log or return the JSON data
+    // console.log(JSON.stringify(creatures, null, 2));
+    return creatures;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+async function getTopTrumps() {
+  const urls = [
+    { url: "https://www.tibiawiki.com.br/wiki/Anf%C3%ADbios" },
+    { url: "https://www.tibiawiki.com.br/wiki/Aqu%C3%A1ticos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Aves" },
+    { url: "https://www.tibiawiki.com.br/wiki/Constructos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Criaturas_M%C3%A1gicas" },
+    { url: "https://www.tibiawiki.com.br/wiki/Dem%C3%B4nios" },
+    { url: "https://www.tibiawiki.com.br/wiki/Drag%C3%B5es" },
+    { url: "https://www.tibiawiki.com.br/wiki/Elementais" },
+    { url: "https://www.tibiawiki.com.br/wiki/Extra_Dimensionais" },
+    { url: "https://www.tibiawiki.com.br/wiki/Fadas" },
+    { url: "https://www.tibiawiki.com.br/wiki/Gigantes" },
+    { url: "https://www.tibiawiki.com.br/wiki/Humanos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Human%C3%B3ides" },
+    { url: "https://www.tibiawiki.com.br/wiki/Licantropos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Mam%C3%ADferos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Mortos-Vivos" },
+    { url: "https://www.tibiawiki.com.br/wiki/Plantas_(Criatura)" },
+    { url: "https://www.tibiawiki.com.br/wiki/R%C3%A9pteis" },
+    { url: "https://www.tibiawiki.com.br/wiki/Slimes" },
+    { url: "https://www.tibiawiki.com.br/wiki/Vermes" },
+  ];
+
+  try {
+    const promises = urls.map((item) => fetchTibiaWikiCreaturesData(item.url));
+    const results = await Promise.all(promises);
+    return results.flat();
+  } catch (error) {
+    console.error("Error fetching creature data:", error);
+  }
+}
+
+const handler = async (event, context) => {
+  try {
+    const data = await getTopTrumps();
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = { handler };
