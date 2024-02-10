9a08c9eada21b6a9d8695d1fd977010e5688e725
ViniciusPaldes <vinnydp@gmail.com>
2024-02-10 12:54:23 -0300

    Firebase service to save and return firestore creatures

--- functions/tibia-wiki-creatures.js
+++ functions/tibia-wiki-creatures.js
@@ -1,108 +1,36 @@
-// Import the required modules
-const axios = require("axios");
-const cheerio = require("cheerio");
 
-function getDifficultyNumber(difficultyString) {
-  switch (difficultyString) {
-    case "Inofensiva":
-      return 0;
-    case "Trivial":
-      return 1;
-    case "Fácil":
-      return 2;
-    case "Mediana":
-      return 3;
-    case "Difícil":
-      return 4;
-    case "Desafiador":
-      return 5;
-    default:
-      return -1;
-  }
-}
 
-async function fetchTibiaWikiCreaturesData(url) {
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase/tibiatoptrumps-firebase-adminsdk-rn9hj-055d43aae0.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tibiatoptrumps-default-rtdb.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+async function getCreatures() {
   try {
-    // Fetch the HTML of the page
-    const { data } = await axios.get(`https://web.archive.org/web/20240201070048/${url}`);
-    const $ = cheerio.load(data);
-    const URL_PREFIX = "https://www.tibiawiki.com.br";
-
-    // The array to hold our creatures data
+    const creaturesRef = db.collection('creatures');
+    const snapshot = await creaturesRef.get();
     const creatures = [];
-
-    // Find each table by ID and loop through its rows
-    $("table#tabelaDPL")
-      .first()
-      .each((_, table) => {
-        // Loop through each row of the table
-        $("tr", table).each((_, tr) => {
-          const tds = $("td", tr).filter(
-            (index, td) => index < $("td", tr).length - 1
-          );
-          if (tds.length > 0) {
-            // Ensure there are columns to process
-            const creature = {
-              name: $(tds[0]).text().trim(),
-              image: `${URL_PREFIX}${$(tds[1]).find("img").attr("src") || ""}`,
-              hp: parseInt($(tds[2]).text().trim(), 10),
-              xp: parseInt($(tds[3]).text().trim(), 10),
-              charms: parseInt($(tds[4]).text().trim(), 10),
-              difficultyString: $(tds[5]).find("img").attr("title"),
-              difficulty: getDifficultyNumber(
-                $(tds[5]).find("img").attr("title")
-              ),
-            };
-            creatures.push(creature);
-          }
-        });
-      });
-
-    // Log or return the JSON data
-    // console.log(JSON.stringify(creatures, null, 2));
+    snapshot.forEach(doc => {
+      creatures.push(doc.data());
+    });
+    console.log(creatures);
     return creatures;
   } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
-
-async function getTopTrumps() {
-  const urls = [
-    { url: "https://www.tibiawiki.com.br/wiki/Anf%C3%ADbios" },
-    { url: "https://www.tibiawiki.com.br/wiki/Aqu%C3%A1ticos" },
-    { url: "https://www.tibiawiki.com.br/wiki/Aves" },
-    { url: "https://www.tibiawiki.com.br/wiki/Constructos" },
-    { url: "https://www.tibiawiki.com.br/wiki/Criaturas_M%C3%A1gicas" },
-    { url: "https://www.tibiawiki.com.br/wiki/Dem%C3%B4nios" },
-    { url: "https://www.tibiawiki.com.br/wiki/Drag%C3%B5es" },
-    { url: "https://www.tibiawiki.com.br/wiki/Elementais" },
-    { url: "https://www.tibiawiki.com.br/wiki/Extra_Dimensionais" },
-    { url: "https://www.tibiawiki.com.br/wiki/Fadas" },
-    { url: "https://www.tibiawiki.com.br/wiki/Gigantes" },
-    { url: "https://www.tibiawiki.com.br/wiki/Humanos" },
-    { url: "https://www.tibiawiki.com.br/wiki/Human%C3%B3ides" },
-    { url: "https://www.tibiawiki.com.br/wiki/Licantropos" },
-    { url: "https://www.tibiawiki.com.br/wiki/Mam%C3%ADferos" },
-    { url: "https://www.tibiawiki.com.br/wiki/Mortos-Vivos" },
-    { url: "https://www.tibiawiki.com.br/wiki/Plantas_(Criatura)" },
-    { url: "https://www.tibiawiki.com.br/wiki/R%C3%A9pteis" },
-    { url: "https://www.tibiawiki.com.br/wiki/Slimes" },
-    { url: "https://www.tibiawiki.com.br/wiki/Vermes" },
-  ];
-
-  try {
-    const promises = urls.map((item) => fetchTibiaWikiCreaturesData(item.url));
-    const results = await Promise.all(promises);
-    return results.flat();
-  } catch (error) {
-    console.error("Error fetching creature data:", error);
+    console.error("Error fetching creatures:", error);
+    throw error; // Propagate the error
   }
 }
 
 const handler = async (event, context) => {
   try {
-    const data = await getTopTrumps();
-    console.log("Data returned from top trumps", data);
+    const data = await getCreatures();
+    // console.log("Data returned from top trumps", data);
     return {
       statusCode: 200,
       headers: {
